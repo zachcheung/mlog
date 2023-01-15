87c61b816c79296dadf47f6f99f3ff9e7aecb339
Zach Cheung <kuroro.zhang@gmail.com>
2023-01-15 21:32:08 +0800

    update doc

--- mlog.go
+++ mlog.go
@@ -12,14 +12,14 @@ const (
 	gray = 37
 )
 
-// A Logger is a standard `log.Logger` with `Debug` family functions.
+// A Logger is a standard log.Logger with Debug family functions.
 type Logger struct {
 	log.Logger
 	isDebug int32 // atomic boolean
 }
 
 // New creates a new Logger and enables the debug log mode when found
-// `DEBUG` environment variable set to one of the following values:
+// DEBUG environment variable set to one of the following values:
 //   - yes
 //   - true
 //   - 1
@@ -37,17 +37,17 @@ func New() *Logger {
 
 var std = New()
 
-// EnableDebug enables the debug log mode and has higher priority than `DEBUG` environment variable.
+// EnableDebug enables the debug log mode and has higher priority than DEBUG environment variable.
 func (l *Logger) EnableDebug() {
 	atomic.StoreInt32(&l.isDebug, 1)
 }
 
-// DisableDebug disables the debug log mode and has higher priority than `DEBUG` environment variable.
+// DisableDebug disables the debug log mode and has higher priority than DEBUG environment variable.
 func (l *Logger) DisableDebug() {
 	atomic.StoreInt32(&l.isDebug, 0)
 }
 
-// Debug prints with `[DEBUG] ` prefix when in the debug mode.
+// Debug prints with "[DEBUG] " prefix when in the debug mode.
 // Arguments are handled in the manner of fmt.Print.
 func (l *Logger) Debug(v ...any) {
 	if atomic.LoadInt32(&l.isDebug) == 1 {
@@ -55,13 +55,13 @@ func (l *Logger) Debug(v ...any) {
 	}
 }
 
-// Debugf prints with `[DEBUG] ` prefix when in the debug mode.
+// Debugf prints with "[DEBUG] " prefix when in the debug mode.
 // Arguments are handled in the manner of fmt.Printf.
 func (l *Logger) Debugf(format string, v ...any) {
 	l.Debug(fmt.Sprintf(format, v...))
 }
 
-// Debugln prints with `[DEBUG] ` prefix when in the debug mode.
+// Debugln prints with "[DEBUG] " prefix when in the debug mode.
 // Arguments are handled in the manner of fmt.Println.
 func (l *Logger) Debugln(v ...any) {
 	l.Debug(fmt.Sprintln(v...))
@@ -71,29 +71,29 @@ func colorize(color int, text string) string {
 	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color, text)
 }
 
-// EnableDebug enables the debug log mode and has higher priority than `DEBUG` environment variable.
+// EnableDebug enables the debug log mode and has higher priority than DEBUG environment variable.
 func EnableDebug() {
 	std.EnableDebug()
 }
 
-// DisableDebug disables the debug log mode and has higher priority than `DEBUG` environment variable.
+// DisableDebug disables the debug log mode and has higher priority than DEBUG environment variable.
 func DisableDebug() {
 	std.DisableDebug()
 }
 
-// Debug prints with `[DEBUG] ` prefix when in the debug mode.
+// Debug prints with "[DEBUG] " prefix when in the debug mode.
 // Arguments are handled in the manner of fmt.Print.
 func Debug(v ...any) {
 	std.Debug(v...)
 }
 
-// Debugf prints with `[DEBUG] ` prefix when in the debug mode.
+// Debugf prints with "[DEBUG] " prefix when in the debug mode.
 // Arguments are handled in the manner of fmt.Printf.
 func Debugf(format string, v ...any) {
 	std.Debugf(format, v...)
 }
 
-// Debugln prints with `[DEBUG] ` prefix when in the debug mode.
+// Debugln prints with "[DEBUG] " prefix when in the debug mode.
 // Arguments are handled in the manner of fmt.Println.
 func Debugln(v ...any) {
 	std.Debugln(v...)
